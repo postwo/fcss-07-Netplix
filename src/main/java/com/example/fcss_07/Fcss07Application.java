05face2904a4b390953b0a792486c239e2fa82aa
postwo <codo7717@naver.com>
2024-12-01 14:57:21 +0900

    세팅

---
+++ src/main/java/com/example/fcss_07/Fcss07Application.java
@@ -0,0 +1,13 @@
+package com.example.fcss_07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fcss07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fcss07Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fcss_07/Fcss07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fcss_07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fcss07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
